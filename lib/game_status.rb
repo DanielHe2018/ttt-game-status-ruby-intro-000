6a0c8a8d0abb55e8eaaaa1d0e967f7f5625d85d0
Daniel He <danielhe.mlp@gmail.com>
2020-09-07 06:27:36 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,6 +17,6 @@ WIN_COMBINATIONS =[
 
 def won? (board)
   WIN_COMBINATIONS.detect do |check|
-    !position_taken?(board,check[0]) && board[check[0]]==board[check[1]] && board[check[1]]==board[check[2]]
+    position_taken?(board,check[0]) && board[check[0]]==board[check[1]] && board[check[1]]==board[check[2]]
   end
 end
